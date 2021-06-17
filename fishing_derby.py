7e92e070db0995962476f60a590ba47d312d4e79
Shan Srinivasan <s.shan.srini@gmail.com>
2021-06-17 11:54:59 -0400

    intermediate work, starting classifier work

--- fishing_derby.py
+++ fishing_derby.py
@@ -3,12 +3,12 @@ import copy
 import random
 
 # CONSTANTS
-ITERATIONS = 100000
+ITERATIONS = 1
 DISCOUNT_FACTOR = 0.8
 EXPLORE_PROB = 0.2 # Eps greedy exploration
 LEARNING_RATE = 0.01
 # is max moves required? Game ends from bot in reasonable time...
-MAX_MOVES = 100000
+MAX_MOVES = 1000
 
 # Load environment for Fishing Derby
 env = gym.make('FishingDerby-v0')
@@ -25,24 +25,87 @@ Q = [[copy.copy(init_q) for __ in range(cols)] for _ in range(rows)]
 # # Box (array dimensional) Box(high=0, low=255, shape=(210, 160, 3), uint8)
 # print(env.observation_space.shape)
 
+lookup_colors = { (167, 26, 26): "red", (24, 26, 167): "blue",
+       (117, 128, 240): "purple", (72, 160, 72): "green",
+       (66, 72, 200): "purple/blue", (232, 232, 74): "yellow",
+       (0, 0, 0): "black", (45, 50, 184): "blue", 
+       (0, 0, 148): "blue", (228, 111, 111): "pink" }
+       
+def get_color(r, g, b):
+    """ Get the string representation of a color """
+    return lookup_colors[(r, g, b)]
+
+def observation_to_colors(arr):
+    """ 2d array of observation to 2d array of colors """
+    ret = [[get_color(rgb[0], rgb[1], rgb[2]) for rgb in row] for row in arr]
+    return ret
+
+def detect_fishing_rod(obs):
+    """ using a 2d array of rgb values, detects the end of the fishing rod
+    @return x: int, y: int of the end of fishing rod
+    """
+    # obs = obs[::-1]
+    # print(obs)
+    MIN_ROD_LENGTH = 2
+    def detect(x, y):
+        """ helper to detect the rod in one spot by checking the pixel is yellow and surrounding is water """
+        if obs[x][y] == 'yellow':
+            left_water = (x - 1 >= 0) and obs[x-1][y] == 'blue'
+            right_water = (x + 1 < len(obs)) and obs[x+1][y] == 'blue'
+            return True
+        return False
+    # iterate observation looking for fishing rod
+    found_rod = False
+    count_rod = 0
+    for rr, row in enumerate(obs):
+        for cc, val in enumerate(row):
+            if detect(rr, cc):
+                found_rod = True
+                count_rod += 1
+            elif found_rod:
+                if count_rod >= MIN_ROD_LENGTH:
+                    return rr - 1, cc - 1
+            else:
+                found_rod = False
+    # raise BaseException("Rod not found")
+
 for _ in range(ITERATIONS):
     done = False
     ii = 0 
     while ii < MAX_MOVES and not done:
         ii += 1
         # epsilon greedy
-        if random.random() < EXPLORE_PROB:
+        if False and random.random() < EXPLORE_PROB:
             action = env.action_space.sample()
         else:
-            action = env.action_space.sample() # find best action given state most likely max(Q[current_hook_x][current_hook_y])
+            action = 5# env.action_space.sample() # find best action given state most likely max(Q[current_hook_x][current_hook_y])
 
         env.render()
         # env.action_space returns all actions, sample picks a random action
         # step returns observation: env.observation_space, reward: float, done: bool, info: dict
         observation, reward, done, info = env.step(action)
-        
+        obs_colors = observation_to_colors(observation)
+        l = detect_fishing_rod(obs_colors)
+        #l.add(f"red: {rgb[0]} green: {rgb[1]} blue: {rgb[2]}")
+        print('step')
+        print(l)
     env.reset()
 env.close()
+
+
+
+"""
+{'red: 167 green: 26 blue: 26', = red
+'red: 24 green: 26 blue: 167' = blue
+'red: 117 green: 128 blue: 240' = purple, 
+'red: 72 green: 160 blue: 72' = green
+'red: 66 green: 72 blue: 200' = purple/blue, 
+'red: 232 green: 232 blue: 74' = yellow, 
+'red: 0 green: 0 blue: 0' = black, 
+'red: 45 green: 50 blue: 184', = blue
+'red: 0 green: 0 blue: 148', = blue
+'red: 228 green: 111 blue: 111'} = pink
+"""
 
 """
     Questions:
